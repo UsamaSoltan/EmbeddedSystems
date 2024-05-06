9b7e179098ff844a56b9e1955c93b036a6927406
osama sultan <osama sultan@gmail.com>
2024-05-06 14:10:36 -0700

    TO RESET

---
+++ c/lec7 T/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ unsigned short y, x=149;
+x&=~(1<<4);
+printf("%d\n",x);
+
+for(int i=7;i>=0;i--)
+{
+    y=((x>>i)&1); // read bit
+printf("%d",y);
+}
+
+    return 0;
+}
